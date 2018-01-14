a0ae9a53f28306335e16ae3e89b497af6800ae44
Asif Kabani <asifkabani@gmail.com>
2018-01-14 15:21:23 -0600

    Updated files

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 
-// Text Analyzer
-/*
-We have three areas which show the word count, unique word count, and average word length. There is a field to input
-text from user. Submit button to analyze the input from the text field.
-*/
-
 /* Globals */
 const wordCountEl = document.getElementById('js-word-count');
 const uniqueCountEl = document.getElementById('js-unique-count');
@@ -17,54 +11,41 @@ const submitButEl = document.getElementById('js-submit');
 
 // Remove non-alphanumerice characters and spaces from user input.
 const removeNonAlpha = (string) => string.toLowerCase().replace(/[^a-z0-9-]+/g, " ").trim();
-console.log(removeNonAlpha(' hi 4545^$@&&%% hi kfsjdlfjsdflkds hello hello%$#%*^&* '))
 
 // Create an array from the clean strings.
 const cleanStrArr = (string) => string.split(" ");
-console.log(cleanStrArr('hi 4545 hi kfsjdfkd hello hello'));
 
 // Create function to get the average length of a word.
 const averageWordLength = (string) => {
-    let stringArr = cleanStrArr(string);
-    let wordCount = stringArr.length;
-    let characterCount = stringArr.sort().join('').length;
-    let getAverage = (characterCount / wordCount).toFixed(2);
-    return getAverage;
+    let wordCount = cleanStrArr(string).length;
+    let totalLetters = cleanStrArr(string).sort().join('').length;
+    return (totalLetters / wordCount).toFixed(2);
 };
-console.log(averageWordLength('hi 4545 hi kfsjdfkd hello hello'));
 
 // Create function to get unique word count. A unique word is one that is not repeated.
-// const uniqueWordCount = (string) => {
-    // let temp = [];
-    // array.forEach((element,index) => {
-    //     if (array.indexOf(element, index + 1) > -1) {
-    //         console.log(index);
-    //         // if (temp.indexOf(element) === -1) {
-    //         //     temp.push(element);
-    //         // }
-    //     }    
-    // });
-    // let resultArr = [];
-    // let origArr = cleanStrArr(string);
-    // origArr.forEach(function(element, index) {
-    //     if (origArr.indexOf(element, index + 1) > -1) {
-    //         if (resultArr.indexOf(element) === -1) {
-    //             resultArr.push(element);
-    //         }
-    //     }
-    // });
-    // return origArr.length - resultArr.length;
-// };
-// console.log(uniqueWordCount(["hi", "4545", "hi", "kfsjdfkd", "hello", "hello"]));
+const uniqueWordCount = (string) => {
+    let tempArr = [];
+    let origArr = cleanStrArr(string);
+    origArr.forEach((element, index) => {
+        if (origArr.indexOf(element, index + 1) > -1) {
+            if (tempArr.indexOf(element) === -1) {
+                tempArr.push(element);
+            }
+        }
+    });
+    return origArr.length - tempArr.length;
+};
 
 // If there is valid input, then process the input.
 const runAnalyzer = () => {
     let cleanStr = removeNonAlpha(userInputEl.value);
     let getWordCount = cleanStrArr(cleanStr).length;
     let getAverageLength = averageWordLength(cleanStr);
+    let getUniqueCount = uniqueWordCount(cleanStr); 
 
     userInputEl.value = cleanStr;
     wordCountEl.value = getWordCount;
+    uniqueCountEl.value = getUniqueCount;
     averageLengthEl.value = getAverageLength;
 };
 
@@ -82,18 +63,8 @@ const checkEmpty = () => {
 };
 
 // Handle form submit
-const handleSubmit = () => {
-    submitButEl.addEventListener('click', checkEmpty);
-};
+const handleSubmit = () => submitButEl.addEventListener('click', checkEmpty);
 handleSubmit();
 
-
-// TESTS
-// When user clicks on icon, show information about web app.
-let navigation = document.getElementById('navigation');
-console.log(navigation)
-// console.log(navigation.style.display = 'block')
-
-/* TODO
-when user enters their text, on analyze, send the value of the 
-*/
+// Handle menu click
+const menuClick = () => menuButEl.addEventListener('click', openMenu)
